41eb03e5fd08e4c5887a53e263c6ec013a61a880
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2023-05-31 18:18:36 +0300

    created validateBodyFavorite

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -38,7 +38,7 @@ const deleteContactById = async (req, res, next) => {
   }
 
   res.json({
-    message: "Delete success",
+    message: "Contact deleted success",
   });
 };
 
@@ -55,7 +55,7 @@ const updateFavorite = async (req, res) => {
   const { id } = req.params;
   const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
   if (!result) {
-    throw HttpError(404, "Not found");
+    throw HttpError(404, `${id} not found`);
   }
   res.json(result);
 };
--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const ctrl = require("../../controllers/contacts-controller");
-const { isValidId, validateBody, authenticate } = require("../../middlewars");
+const {
+  isValidId,
+  validateBody,
+  authenticate,
+  validateBodyFavorite,
+} = require("../../middlewars");
 const { schemas } = require("../../models/contact");
 
 const router = express.Router();
@@ -25,7 +30,7 @@ router.put(
 router.patch(
   "/:id/favorite",
   isValidId,
-  validateBody(schemas.uptadeFavoriteSchema),
+  validateBodyFavorite(schemas.uptadeFavoriteSchema),
   ctrl.updateFavorite
 );
 
